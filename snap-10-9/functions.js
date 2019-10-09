02efbe36bd5464473f5f706826681c3faf5b02d1
Floribella Ponce <fponce2@cnm.edu>
2019-10-09 09:57:13 -0600

    added two functions

---
+++ snap-10-9/functions.js
@@ -0,0 +1,12 @@
+/* Addition Function */
+function functionAdd (param) {
+	return param + param
+}
+console.log(functionAdd(7, 8));
+
+
+function concatStrings(firstName, lastName) {
+	return firstName + " " + lastName
+	//Return firstName concat(lastName)
+}
+console.log(concatStrings("Bella", "Ponce"));
